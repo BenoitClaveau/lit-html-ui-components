dd725759b50159e8453ec4fa9ba756d8d854703f
Benoît Claveau <benoit.claveau@gmail.com>
2020-01-14 14:41:22 +0100

    files

---
+++ index.js
@@ -0,0 +1,6 @@
+export { default as ModalDialog } from './src/modal-dialog';
+export { default as AutoComplete } from './src/auto-complete';
+export { default as InputText } from './src/auto-complete';
+export { default as ReadonlyText } from './src/auto-complete';
+
+
